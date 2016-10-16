cda6d88e0ffc5a0ee47aff273e5ded976a7aae7d
Kim Stephenson <kmstephe@usc.edu>
2016-10-15 17:57:10 -0700

    Render something on page from API call

--- src/scripts/components/CompanyData.js
+++ src/scripts/components/CompanyData.js
@@ -2,12 +2,14 @@ var React = require('react')
 import $ from 'jquery'
 
 var CompanyData = React.createClass({
+  getInitialState: function() {
+    return {data: []}
+  },
   componentWillMount: function() {
     $.ajax({
       url: "https://insiderapi.herokuapp.com/forms",
       dataType: "json",
       success: function(data) {
-        console.log(data)
         this.setState({data: data})
       }.bind(this),
       error: function(xhr, status, err) {
@@ -17,7 +19,13 @@ var CompanyData = React.createClass({
   },
   render: function() {
     return(
-      <p>{this.state.data}</p>
+      <ul className="TestList">
+      {
+        this.state.data.map(function(form) {
+        return <li key={form.id}>{form.insider_id}</li>
+        })
+      }
+      </ul>
     )
   }
 })
