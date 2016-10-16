a908bba8ce90e1b46d623794e1f3092e22567e5a
Kim Stephenson <kmstephe@usc.edu>
2016-10-16 11:14:56 -0700

    Make API call to companies index

--- src/scripts/components/HeatMap.js
+++ src/scripts/components/HeatMap.js
@@ -1,6 +1,23 @@
 var React = require('react')
+import $ from 'jquery'
 
 var HeatMap = React.createClass({
+  getInitialState: function() {
+    return {data: []}
+  },
+  componentWillMount: function() {
+    $.ajax({
+      url: "https://insiderapi.herokuapp.com",
+      dataType: "json",
+      success: function(data) {
+        console.log(data)
+        this.setState({data: data})
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this, status, err.toString())
+      }.bind(this)
+    })
+  },
   render: function() {
     return(
       <div className="HeatMap">
