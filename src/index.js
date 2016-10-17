b872f6cc8c4eb4f25521d491d2ad36970cdc48db
Izzy Matos <israeldmatos@gmail.com>
2016-10-17 14:57:37 -0700

    Add import and render Search component to index js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './scripts/components/app';
 import CompanyShow from './scripts/components/CompanyShow';
+import Search from './scripts/components/Search'
 import './css/index.css';
 import { Router, Route, browserHistory } from 'react-router'
 
@@ -9,6 +10,7 @@ ReactDOM.render((
     <Router history={browserHistory}>
       <Route path="/" component={App}/>
       <Route path="/companies/:companyName" component={CompanyShow}/>
+      <Route path="/search" component={Search}/>
     </Router>
   ), document.getElementById('root'))
 
