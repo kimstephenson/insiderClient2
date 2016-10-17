4676fd6279addcb349e4bdc45a8ee5bf6e3b8f37
Izzy Matos <israeldmatos@gmail.com>
2016-10-16 18:39:28 -0700

    Add import and render NewsFeed component in top CompanyShow component

--- src/scripts/components/CompanyShow.js
+++ src/scripts/components/CompanyShow.js
@@ -1,12 +1,14 @@
 var React = require ('react')
 import CompanyHeader from './CompanyHeader'
 import CompanyData from './CompanyData'
+import NewsFeed from './NewsFeed'
 
 var CompanyShow = React.createClass({
   render: function() {
     return(
       <div className="companyShow">
         <CompanyHeader companyName={this.props.params.companyName}/>
+        <NewsFeed companyName={this.props.params.companyName} />
         <CompanyData/>
       </div>
     )
