d5134f15a8986a99f4030097c7ba0f2c4707f38b
Izzy Matos <israeldmatos@gmail.com>
2016-10-17 03:04:06 -0700

    Fix variables namespace

--- src/scripts/components/News.js
+++ src/scripts/components/News.js
@@ -12,7 +12,7 @@ const News = React.createClass({
     var url = `https://webhose.io/search?token=dc880ca7-8a9b-4f5c-8f99-d246a8361ae5&format=json&q=thread.title%3A(${query})%20language%3A(english)%20site_category%3Afinance%20organization%3A%22${query}%22%20(site_type%3Anews%20OR%20site_type%3Ablogs)&ts=1476079876068`
     Request.get(url).then((response) => {
       this.setState({
-        posts: response.body.posts.slice(0, 5)
+        articles: response.body.posts.slice(0, 5)
       })
     })
   },
@@ -22,17 +22,17 @@ const News = React.createClass({
   },
 
   render: function() {
-    var posts = _.map(this.state.posts, (post) => {
+    var articles = _.map(this.state.articles, (article) => {
       return (
         <li>
-          <a href={post.thread.url}>
-            <img className='news-img' src={post.thread.main_image} alt='main-img' />
+          <a href={article.thread.url}>
+            <img className='news-img' src={article.thread.main_image} alt='main-img' />
           </a>
           <h3>
-            <a href={post.thread.url} >{post.title}</a>
+            <a href={article.thread.url} >{article.title}</a>
           </h3>
-          <p><span>{post.published}</span></p>
-          <p>{post.text}</p>
+          <p><span>{article.published}</span></p>
+          <p>{article.text}</p>
         </li>
       )
     })
@@ -44,8 +44,8 @@ const News = React.createClass({
                 return f.toUpperCase()
               })
             }
-      </h3>
-        <ul>{posts}</ul>
+        </h3>
+        <ul>{articles}</ul>
       </div>
     )
   }
