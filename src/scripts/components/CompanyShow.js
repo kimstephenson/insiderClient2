96b3fb0496d11ee29cccf3e654228aa7dbd26b16
Kim Stephenson <kmstephe@usc.edu>
2016-10-18 19:34:27 -0700

    Render highcharts bubble chart

--- src/scripts/components/CompanyShow.js
+++ src/scripts/components/CompanyShow.js
@@ -1,11 +1,87 @@
 var React = require ('react')
+var ReactHighcharts = require('react-highcharts')
+var Highcharts = require('highcharts')
+var HighchartsMore = require('highcharts-more')(ReactHighcharts.Highcharts)
 import CompanyHeader from './CompanyHeader'
 import CompanyData from './CompanyData'
 import NewsFeed from './NewsFeed'
 import TransactionChart from './TransactionChart'
 
-var CompanyShow = React.createClass({
+var chartOptions = { chart: {
+            type: 'bubble',
+            plotBorderWidth: 1,
+            zoomType: 'xy'
+        },
 
+        title: {
+            text: 'Transactions'
+        },
+
+        xAxis: {
+            gridLineWidth: 1
+        },
+
+        yAxis: {
+            startOnTick: false,
+            endOnTick: false
+        },
+
+        series: [{
+            data: [
+                [9, 81, 63],
+                [98, 5, 89],
+                [51, 50, 73],
+                [41, 22, 14],
+                [58, 24, 20],
+                [78, 37, 34],
+                [55, 56, 53],
+                [18, 45, 70],
+                [42, 44, 28],
+                [3, 52, 59],
+                [31, 18, 97],
+                [79, 91, 63],
+                [93, 23, 23],
+                [44, 83, 22]
+            ],
+            marker: {
+                fillColor: {
+                    radialGradient: { cx: 0.4, cy: 0.3, r: 0.7 },
+                    stops: [
+                        [0, 'rgba(255,255,255,0.5)'],
+                        [1, Highcharts.Color(Highcharts.getOptions().colors[8]).setOpacity(0.5).get('rgba')]
+                    ]
+                }
+            }
+        }, {
+            data: [
+                [42, 38, 20],
+                [6, 18, 1],
+                [1, 93, 55],
+                [57, 2, 90],
+                [80, 76, 22],
+                [11, 74, 96],
+                [88, 56, 10],
+                [30, 47, 49],
+                [57, 62, 98],
+                [4, 16, 16],
+                [46, 10, 11],
+                [22, 87, 89],
+                [57, 91, 82],
+                [45, 15, 98]
+            ],
+            marker: {
+                fillColor: {
+                    radialGradient: { cx: 0.4, cy: 0.3, r: 0.7 },
+                    stops: [
+                        [0, 'rgba(255,255,255,0.5)'],
+                        [1, Highcharts.Color(Highcharts.getOptions().colors[2]).setOpacity(0.5).get('rgba')]
+                    ]
+                }
+            }
+        }]
+     }
+
+var CompanyShow = React.createClass({
   render: function() {
     return(
       <div className="companyShow">
@@ -13,6 +89,7 @@ var CompanyShow = React.createClass({
         <TransactionChart companyName={this.props.params.companyName} />
         <NewsFeed companyName={this.props.params.companyName} />
         <CompanyData/>
+        <ReactHighcharts config={chartOptions} ref="chart"/>
       </div>
     )
   }
