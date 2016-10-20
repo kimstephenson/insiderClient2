d86b215e531f1e1b4ff0e179b2e0771a4724b50a
Izzy Matos <israeldmatos@gmail.com>
2016-10-20 04:20:32 -0700

    Add company stats to SearchCard component

--- src/scripts/components/SearchComponents/SearchCard.js
+++ src/scripts/components/SearchComponents/SearchCard.js
@@ -1,10 +1,15 @@
 import React from 'react'
 
-const SearchCard = ({gif, onGifSelect}) => {
+const SearchCard = ({company, onCardSelect}) => {
   return (
-    <div className="search-card" onClick={() => onGifSelect(gif)}>
-      <img src={gif.images.downsized.url} />
-    </div>
+    <a href={`/companies/${company.name}`}>
+      <div className="search-card" onClick={() => onCardSelect(company)}>
+        <img src={'http://fakeimg.pl/300'} />
+        <h2>{company.name}</h2>
+        <h3>{company.transactions_total}</h3>
+        <p>Traded this quarter</p>
+        <p>{company.insiders} insiders</p>
+      </div></a>
   )
 }
 
