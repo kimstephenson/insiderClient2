11081617c074dd876af941475d4e6e4648326409
Izzy Matos <israeldmatos@gmail.com>
2016-10-19 22:41:35 -0700

    Refactor SearchResults to render CompanyCard

--- src/scripts/components/SearchComponents/SearchResults.js
+++ src/scripts/components/SearchComponents/SearchResults.js
@@ -1,32 +1,14 @@
 import React from 'react'
-<<<<<<< HEAD
 import CompanyCard from './CompanyCard'
-=======
->>>>>>> development
 
 var SearchResults = React.createClass({
   render: function() {
     return (
-<<<<<<< HEAD
       this.props.results.map(function(co) {
         return(
           <CompanyCard co={co} />
         )
       })
-=======
-      <div>
-        {this.props.results.map(function(co) {
-          return (
-            <div key={co.id}>
-              <a href={`/companies/${co.name}`}>{co.name}</a>
-              <p>Total insiders: {co.insiders}</p>
-              <p>Monthly Transactions: {co.monthly_total}</p>
-            </div>
-          )
-        })
-      }
-      </div>
->>>>>>> development
     )
   }
 })
