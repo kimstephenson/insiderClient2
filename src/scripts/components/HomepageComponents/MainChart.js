71b704d34fc657c2f71e932fc1ac57b2852d6f94
Kim Stephenson <kmstephe@usc.edu>
2016-10-19 18:27:29 -0700

    Format homepage chart for our data set

--- src/scripts/components/HomepageComponents/MainChart.js
+++ src/scripts/components/HomepageComponents/MainChart.js
@@ -10,73 +10,73 @@ var HighchartsTreemap = require('highcharts-treemap')(ReactHighcharts.Highcharts
                     '2016-09-23': '890',
                     '2016-09-24': '250'
                 },
-                'Bangladesh': {
-                    'Noncommunicable diseases': '548.9',
-                    'Injuries': '64.0',
-                    'Communicable & other Group I': '234.6'
+                'Some Person': {
+                    '2016-09-22': '548.9',
+                    '2016-09-23': '64.0',
+                    '2016-09-24': '234.6'
                 },
-                'Myanmar': {
-                    'Communicable & other Group I': '316.4',
-                    'Injuries': '102.0',
-                    'Noncommunicable diseases': '708.7'
+                'Other Guy': {
+                    '2016-09-22': '316.4',
+                    '2016-09-23': '102.0',
+                    '2016-09-24': '708.7'
                 }
             },
             'Apple': {
-                'Hungary': {
-                    'Communicable & other Group I': '16.8',
-                    'Noncommunicable diseases': '602.8',
-                    'Injuries': '44.3'
+                'Tim Cook': {
+                    '2016-09-22': '16.8',
+                    '2016-09-23': '602.8',
+                    '2016-09-24': '44.3'
                 },
-                'Poland': {
-                    'Communicable & other Group I': '22.6',
-                    'Noncommunicable diseases': '494.5',
-                    'Injuries': '48.9'
+                'Me': {
+                    '2016-09-22': '22.6',
+                    '2016-09-23': '494.5',
+                    '2016-09-24': '48.9'
                 }
             },
             'IBM': {
-                'Equatorial Guinea': {
-                    'Communicable & other Group I': '756.8',
-                    'Injuries': '133.6',
-                    'Noncommunicable diseases': '729.0'
+                'Somebody': {
+                    '2016-09-22': '756.8',
+                    '2016-09-23': '133.6',
+                    '2016-09-24': '729.0'
                 },
-                'Madagascar': {
-                    'Noncommunicable diseases': '648.6',
-                    'Communicable & other Group I': '429.9',
-                    'Injuries': '89.0'
+                'Someone else': {
+                    '2016-09-22': '648.6',
+                    '2016-09-23': '429.9',
+                    '2016-09-24': '89.0'
                 },
-                'Swaziland': {
-                    'Communicable & other Group I': '884.3',
-                    'Injuries': '119.5',
-                    'Noncommunicable diseases': '702.4'
+                'Random Person': {
+                    '2016-09-22': '884.3',
+                    '2016-09-23': '119.5',
+                    '2016-09-24': '702.4'
                 },
-                'Congo': {
-                    'Noncommunicable diseases': '632.3',
-                    'Communicable & other Group I': '666.9',
-                    'Injuries': '89.0'
+                'Stuff': {
+                    '2016-09-22': '632.3',
+                    '2016-09-23': '666.9',
+                    '2016-09-24': '89.0'
                 }
             },
             'Splunk INC': {
-                'Hungary': {
-                    'Communicable & other Group I': '16.8',
-                    'Noncommunicable diseases': '602.8',
-                    'Injuries': '44.3'
+                'John Doe': {
+                    '2016-09-22': '16.8',
+                    '2016-09-23': '602.8',
+                    '2016-09-24': '440.3'
                 },
-                'Poland': {
-                    'Communicable & other Group I': '22.6',
-                    'Noncommunicable diseases': '494.5',
-                    'Injuries': '48.9'
+                'Jane Doe': {
+                    '2016-09-22': '22.6',
+                    '2016-09-23': '494.5',
+                    '2016-09-24': '48.9'
                 }
             },
             'CBS': {
-                'Hungary': {
-                    'Communicable & other Group I': '16.8',
-                    'Noncommunicable diseases': '602.8',
-                    'Injuries': '44.3'
+                'Bob': {
+                    '2016-09-22': '16.8',
+                    '2016-09-23': '602.8',
+                    '2016-09-24': '44.3'
                 },
-                'Poland': {
-                    'Communicable & other Group I': '22.6',
-                    'Noncommunicable diseases': '494.5',
-                    'Injuries': '48.9'
+                'Tom': {
+                    '2016-09-22': '22.6',
+                    '2016-09-23': '494.5',
+                    '2016-09-24': '48.9'
                 }
             }
         },
@@ -119,7 +119,7 @@ for (company in data) {
                         if (data[company][insider].hasOwnProperty(cause)) {
                             causeP = {
                                 id: insiderP.id + '_' + causeI,
-                                name: causeName[cause],
+                                name: cause,
                                 parent: insiderP.id,
                                 value: Math.round(+data[company][insider][cause])
                             };
