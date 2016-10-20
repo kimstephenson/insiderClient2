e60cc73db4f52b7d8370c8fd4892cbf168929432
Izzy Matos <israeldmatos@gmail.com>
2016-10-20 11:10:25 -0700

    Merge changes from dev

--- src/scripts/components/SearchComponents/SearchBar.js
+++ src/scripts/components/SearchComponents/SearchBar.js
@@ -1,19 +1,32 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 class SearchBar extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { term: ''}
+    this.state = { searchTerm: ''}
   }
 
   onInputChange(term) {
     this.setState({term})
     this.props.onTermChange(term)
   }
+
+  onInputSubmit(e) {
+    this.props.onSubmit(e)
+  }
+
+ 
+
   render() {
     return (
       <div>
+<<<<<<< Updated upstream
         <input className="searchBar" placeholder='Search company or ticker' onChange={event => this.onInputChange(event.target.value)} />
+=======
+        <input className="searchBar" placeholder='Search company or ticker' onChange={this.handleSearchTermEvent}  />
+        <Link to='/search' className='searchBtn'> Browse </Link>
+>>>>>>> Stashed changes
       </div>
     )
   }
