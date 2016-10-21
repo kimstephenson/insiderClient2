028c0d88d8376dabeb0d645cce836b68bd88334c
Izzy Matos <israeldmatos@gmail.com>
2016-10-20 22:16:29 -0700

    Add state and event listener methods to pass down as props

--- src/Layout.js
+++ src/Layout.js
@@ -2,10 +2,18 @@ import React from 'react'
 import Header from './Header'
 
 const Layout = React.createClass ({
+  getInitialState() {
+    return { searchTerm: '' }
+  },
+
+  handleSearchTermChange: function(event) {
+    this.setState({ searchTerm: event.target.value })
+  },
+
   render: function() {
     return(
       <div className='app-container'>
-        <Header />
+        <Header searchTerm={this.state.searchTerm} handleSearchTermChange={this.handleSearchTermChange} />
         {this.props.children}
       </div>
     )
