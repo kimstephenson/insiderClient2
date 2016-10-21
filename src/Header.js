cc98e12087ee809eb47c17736ef09ed01491b36b
Izzy Matos <israeldmatos@gmail.com>
2016-10-20 22:15:52 -0700

    Add key press event listener and import browserHistory

--- src/Header.js
+++ src/Header.js
@@ -1,17 +1,23 @@
 const React = require('react')
 const { Link } = require('react-router')
+import { browserHistory } from 'react-router';
 import SearchBar from './scripts/components/SearchComponents/SearchBar'
 import './css/Header.css'
 
 var Header = React.createClass({
   getInitialState () {
-    return {
-      searchTerm: ''
-    }
+    return { searchTerm: '' }
   },
 
   handleSearchTermEvent (event) {
     this.props.handleSearchTermChange(event.target.value)
+  },
+
+  handleKeyPress: function(event) {
+    if (event.key === 'Enter') {
+      console.log('router')
+      browserHistory.push(`/search/${this.state.searchTerm}`)  
+    }
   },
 
   render: function() {
@@ -22,7 +28,7 @@ var Header = React.createClass({
             <div className="logo-title">Smarter Bear</div>
           </Link>
 
-        <input type='text' className='searchBar' placeholder='Search by company or ticker' value={this.props.searchTerm} onChange={this.handleSearchTermEvent} />
+        <input type='text' className='searchBar' placeholder='Search by company or ticker' value={this.props.searchTerm} onChange={this.handleSearchTermEvent} onKeyPress={this.handleKeyPress} />
       </header>
     )
   }
